efac22ed03fcb9d76d3ebd00341099f8b614ae2a
Zahiitg <155324337+Zahiitg@users.noreply.github.com>
2026-05-18 20:05:18 +0700

    Update StageContentDto.java

--- lucy-content-service/src/main/java/com/project/Lucy/dto/StageContentDto.java
+++ lucy-content-service/src/main/java/com/project/Lucy/dto/StageContentDto.java
@@ -1,1 +1,8 @@
+import java.util.List;
 
+public class StageContentDto {
+    private int stageNumber;
+    private String stageName;
+    private List<LevelContentDto> levels;
+    // getters, setters
+}
