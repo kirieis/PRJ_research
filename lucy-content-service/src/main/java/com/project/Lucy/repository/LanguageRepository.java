5d8d51f07eeaf6592e449e7a83a511ac3afceb11
Zahiitg <155324337+Zahiitg@users.noreply.github.com>
2026-05-22 13:13:29 +0700

    Update LanguageRepository.java

--- lucy-content-service/src/main/java/com/project/Lucy/repository/LanguageRepository.java
+++ lucy-content-service/src/main/java/com/project/Lucy/repository/LanguageRepository.java
@@ -2,8 +2,7 @@ package com.project.Lucy.repository;
 
 import com.project.Lucy.entity.Language;
 import org.springframework.data.jpa.repository.JpaRepository;
-import java.util.Optional;
 
 public interface LanguageRepository extends JpaRepository<Language, Long> {
-    Optional<Language> findByCode(String code);
+    Language findByCode(String code);
 }
