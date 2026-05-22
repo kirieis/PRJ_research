752a0a6ed397ca43cdc0a497fd738988d7d68154
Zahiitg <155324337+Zahiitg@users.noreply.github.com>
2026-05-22 14:26:53 +0700

    Add method to find ContentItems ordered by index

--- lucy-content-service/src/main/java/com/project/Lucy/repository/ContentItemRepository.java
+++ lucy-content-service/src/main/java/com/project/Lucy/repository/ContentItemRepository.java
@@ -7,4 +7,5 @@ import java.util.List;
 public interface ContentItemRepository extends JpaRepository<ContentItem, Long> {
     ContentItem findBySubLevelIdAndOrderIndex(Long subLevelId, Integer orderIndex);
     List<ContentItem> findBySubLevelId(Long subLevelId);
+    List<ContentItem> findBySubLevelIdOrderByOrderIndex(Long subLevelId);
 }
