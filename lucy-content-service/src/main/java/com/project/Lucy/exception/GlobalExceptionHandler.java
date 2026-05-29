f27a687cde40f822d0ab5b9088c8395fc93ad44b
Sand036 <lquan0300@gmail.com>
2026-05-29 10:59:23 +0700

    Create GlobalExceptionHandler.java

---
+++ lucy-content-service/src/main/java/com/project/Lucy/exception/GlobalExceptionHandler.java
@@ -0,0 +1,20 @@
+package com.project.Lucy.exception;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+import java.util.Map;
+
+@RestControllerAdvice
+public class GlobalExceptionHandler {
+
+    @ExceptionHandler(RuntimeException.class)
+    @ResponseStatus(HttpStatus.NOT_FOUND)
+    public Map<String, String> handleNotFound(RuntimeException ex) {
+        return Map.of(
+                "error", ex.getMessage(),
+                "status", "404");
+    }
+}
